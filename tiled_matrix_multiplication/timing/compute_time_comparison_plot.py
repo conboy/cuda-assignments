152522c9a6a6e61c7eccd8c6bcd61e276a5cf750
conboy <conjamalex@gmail.com>
2024-03-27 21:14:30 -0400

    Wrote comparison in MP2 Part 1

---
+++ tiled_matrix_multiplication/timing/compute_time_comparison_plot.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('tiled_matrix_multiplication\\timing\compute_time_comparison.csv')
+
+
+# Grouping the data by 'Matrix Size' and 'Tile Size', then calculating the mean and standard deviation
+grouped = data.groupby(['Matrix Size', 'Tile Size'])['Matrix Multiplication Compute Time'].agg(['mean', 'std']).reset_index()
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+# Getting unique matrix sizes for different lines
+matrix_sizes = grouped['Matrix Size'].unique()
+
+for matrix_size in matrix_sizes:
+    subset = grouped[grouped['Matrix Size'] == matrix_size]
+    plt.errorbar(subset['Tile Size'], subset['mean'], yerr=subset['std'], label=f'Matrix Size {matrix_size}', capsize=5, marker='o')
+
+plt.xlabel('Tile Size')
+plt.ylabel('Matrix Multiplication Compute Time')
+plt.title('Matrix Multiplication Compute Time by Tile Size and Matrix Size')
+plt.legend()
+plt.grid(True)
+plt.show()
